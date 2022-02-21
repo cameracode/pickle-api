041b42eb174572624a4d29efe6684c2dddb36078
camerajunkie <ebenofthecamera@gmail.com>
2022-02-20 23:27:05 -0500

    Fix gen.go
    Had some incorrect syntax on generate swagger.

--- pkg/swagger/gen.go
+++ pkg/swagger/gen.go
@@ -2,4 +2,4 @@ package swagger
 
 //go:generate rm-rf server
 //go:generate mkdir -p server
-//go:generate swagger generate-server --quiet --target server --name pickle-api --spec swagger.yml --exclude-main
+//go:generate swagger generate server --quiet --target server --name pickle-api --spec swagger.yml --exclude-main
